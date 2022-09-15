669dea2e88b8def8b5a3d25321cb5ddeccec202c
oomaar <omaar_5@hotmail.com>
2022-09-15 21:39:47 +0200

    setup Home Screen

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { TwitterSidebar } from "./Screens";
+import { Home, TwitterSidebar } from "./Screens";
 
 export const App = () => {
   return (
     <div>
       <Router basename="/">
         <Routes>
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<Home />} />
           <Route path="/twitter" element={<TwitterSidebar />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </div>
---
+++ src/Screens/Home/styledHome.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div``;
+
+export const HomeNavbar = styled.nav``;
+
+export const HomeLogo = styled.div`
+  h1 {
+  }
+`;
+
+export const HomeLinksList = styled.ul``;
+
+export const HomeLinksListItem = styled.li`
+  a {
+  }
+`;
