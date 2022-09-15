12220982a00d069c18ce916e593df143a5056e55
oomaar <omaar_5@hotmail.com>
2022-09-15 23:56:17 +0200

    add react logo

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,5 +1,20 @@
 import { HomeContainer } from "./styledHome";
 
 export const Home = () => {
-  return <HomeContainer>Home</HomeContainer>;
+  return (
+    <HomeContainer>
+      <h1>Welcome to React Challenge</h1>
+      <p>
+        In this project I will complete some challenges to improve my skills.{" "}
+        <br /> Resourse is:
+        <a
+          href="https://www.fastcomet.com/blog/what-can-i-build-with-react-js"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Here
+        </a>
+      </p>
+    </HomeContainer>
+  );
 };
