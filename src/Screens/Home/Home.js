64645543afd069c7aa8a49ff988980ce0e3aa867
oomaar <omaar_5@hotmail.com>
2022-09-16 00:31:05 +0200

    design and style Home Screen

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,20 +1,31 @@
-import { HomeContainer } from "./styledHome";
+import {
+  HeroIconContainer,
+  HeroTextContainer,
+  HomeContainer,
+} from "./styledHome";
 
 export const Home = () => {
   return (
     <HomeContainer>
-      <h1>Welcome to React Challenge</h1>
-      <p>
-        In this project I will complete some challenges to improve my skills.{" "}
-        <br /> Resourse is:
-        <a
-          href="https://www.fastcomet.com/blog/what-can-i-build-with-react-js"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Here
-        </a>
-      </p>
+      <HeroTextContainer>
+        <h1>
+          Welcome to <span>React Challenge</span>
+        </h1>
+        <p>
+          In this project I will complete some challenges to improve my skills.{" "}
+          <br /> Resourse is:{" "}
+          <a
+            href="https://www.fastcomet.com/blog/what-can-i-build-with-react-js"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Here
+          </a>
+        </p>
+      </HeroTextContainer>
+      <HeroIconContainer>
+        <img src="/react.png" alt="" />
+      </HeroIconContainer>
     </HomeContainer>
   );
 };
--- src/Screens/Home/styledHome.js
+++ src/Screens/Home/styledHome.js
@@ -1,3 +1,85 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { lgScreen, mdScreen } from "../../Global/GlobalStyles";
 
-export const HomeContainer = styled.div``;
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 120px);
+
+  @media screen and (max-width: ${lgScreen}) {
+    flex-direction: column;
+  }
+`;
+
+export const HeroTextContainer = styled.div`
+  color: ${({ theme }) => theme.colors.titleColor};
+
+  h1 {
+    font-size: 3rem;
+    width: 350px;
+    margin: 0;
+
+    span {
+      color: ${({ theme }) => theme.colors.success};
+    }
+
+    @media screen and (max-width: ${mdScreen}) {
+      font-size: 2rem;
+      width: 100%;
+    }
+  }
+
+  p {
+    margin: 0;
+    font-size: 1.5rem;
+    width: 350px;
+
+    a {
+      color: ${({ theme }) => theme.colors.primaryColor};
+
+      :hover {
+        text-decoration: underline;
+      }
+    }
+
+    @media screen and (max-width: ${mdScreen}) {
+      font-size: 1.2rem;
+      width: 100%;
+    }
+  }
+`;
+
+export const HeroIconContainer = styled.div`
+  font-size: 15rem;
+  color: ${({ theme }) => theme.colors.success};
+  transform-origin: center center;
+  -moz-animation: ${rotate} 5s infinite linear;
+  -webkit-animation: ${rotate} 5s infinite linear;
+  animation: ${rotate} 5s infinite linear;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 100%;
+  }
+
+  @media screen and (max-width: ${mdScreen}) {
+    width: 150px;
+    height: 150px;
+    margin-top: 1rem;
+  }
+`;
