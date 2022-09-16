5e7bfc54a2099f00e3bf92eb68bb18d62d0522a4
oomaar <omaar_5@hotmail.com>
2022-09-16 20:13:24 +0200

    setup and style TwitterSidebar

--- src/Screens/TwitterSidebar/TwitterSidebar.js
+++ src/Screens/TwitterSidebar/TwitterSidebar.js
@@ -1,5 +1,77 @@
-import { TwitterAside } from "./styledTwitterSidebar";
+import twitterSidebarData from "./twitterSidebarData.json";
+import {
+  TwitterAside,
+  TwitterLogo,
+  SidebarLinksList,
+  SidebarLinksListItem,
+  TweetButton,
+  SidebarProfile,
+  SidebarProfileImage,
+  SidebarProfileTextContainer,
+  SidebarProfileOptions,
+  SidebarProfileOptionsHeader,
+  SidebarProfileOptionsBody,
+} from "./styledTwitterSidebar";
+import { useState } from "react";
 
 export const TwitterSidebar = () => {
-  return <TwitterAside>TwitterSidebar</TwitterAside>;
+  const [showProfileOptions, setShowProfileOptions] = useState(false);
+
+  const links = twitterSidebarData.map((link) => (
+    <SidebarLinksListItem key={link.id}>
+      <div
+        className={
+          link.name === "More"
+            ? "more"
+            : link.name === "Notifications"
+            ? "notifications"
+            : ""
+        }
+      >
+        <i className={link.icon} />
+      </div>
+      <p>{link.name}</p>
+    </SidebarLinksListItem>
+  ));
+
+  return (
+    <TwitterAside>
+      <TwitterLogo>
+        <i className="bx bxl-twitter" />
+      </TwitterLogo>
+      <SidebarLinksList>{links}</SidebarLinksList>
+      <TweetButton>Tweet</TweetButton>
+      <SidebarProfile onClick={() => setShowProfileOptions((state) => !state)}>
+        <SidebarProfileImage>
+          <i className="bx bx-user" />
+        </SidebarProfileImage>
+        <SidebarProfileTextContainer>
+          <h3>Full Name</h3>
+          <p>@username</p>
+        </SidebarProfileTextContainer>
+        <div>
+          <i className="bx bx-dots-horizontal-rounded" />
+        </div>
+      </SidebarProfile>
+
+      <SidebarProfileOptions showProfileOptions={showProfileOptions}>
+        <SidebarProfileOptionsHeader>
+          <SidebarProfileImage>
+            <i className="bx bx-user" />
+          </SidebarProfileImage>
+          <SidebarProfileTextContainer>
+            <h3>Full Name</h3>
+            <p>@username</p>
+          </SidebarProfileTextContainer>
+          <div>
+            <i className="bx bx-check" />
+          </div>
+        </SidebarProfileOptionsHeader>
+        <SidebarProfileOptionsBody>
+          <p>Add an existing account</p>
+          <p>Log out @username</p>
+        </SidebarProfileOptionsBody>
+      </SidebarProfileOptions>
+    </TwitterAside>
+  );
 };
--- src/Screens/TwitterSidebar/styledTwitterSidebar.js
+++ src/Screens/TwitterSidebar/styledTwitterSidebar.js
@@ -1,3 +1,177 @@
 import styled from "styled-components";
 
-export const TwitterAside = styled.aside``;
+export const TwitterAside = styled.aside`
+  width: 250px;
+  height: 100vh;
+  padding: 0.5rem;
+  border-right: 2px solid ${({ theme }) => theme.colors.borderColor};
+  color: ${({ theme }) => theme.colors.titleColor};
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.bodyColor};
+  z-index: ${({ theme }) => theme.index.top};
+`;
+
+export const TwitterLogo = styled.div`
+  font-size: 2rem;
+  padding: 0 0.5rem;
+`;
+
+export const SidebarLinksList = styled.ul``;
+
+export const SidebarLinksListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 0 1rem;
+  cursor: pointer;
+  padding: 0.3rem;
+  border-radius: 0.5rem;
+  transition: ${({ theme }) => theme.transition.fast};
+
+  i {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+    margin: 0;
+  }
+
+  div.more {
+    border: 1px solid ${({ theme }) => theme.colors.titleColor};
+    width: 30px;
+    height: 30px;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div.notifications {
+    position: relative;
+
+    ::after {
+      content: "20+";
+      background: ${({ theme }) => theme.colors.primaryColor};
+      width: 35px;
+      font-size: 0.6rem;
+      border-radius: 30%;
+      text-align: center;
+      position: absolute;
+      left: 0;
+      top: -4px;
+    }
+  }
+
+  :hover {
+    background-color: ${({ theme }) => theme.colors.borderColor};
+  }
+`;
+
+export const TweetButton = styled.button`
+  width: 100%;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.titleColor};
+  background: ${({ theme }) => theme.colors.primaryColor};
+  transition: ${({ theme }) => theme.transition.fast};
+  border: 0;
+  outline: 0;
+  font-weight: 600;
+  letter-spacing: 1px;
+  font-size: 1.2rem;
+  margin-top: 2rem;
+
+  :hover {
+    background: ${({ theme }) => theme.colors.warning};
+    color: ${({ theme }) => theme.colors.bodyColor};
+  }
+`;
+
+export const SidebarProfile = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 5rem auto 0;
+  justify-content: space-around;
+  width: 200px;
+  transition: ${({ theme }) => theme.transition.fast};
+  cursor: pointer;
+  border-radius: 0.5rem;
+
+  :hover {
+    background: ${({ theme }) => theme.colors.borderColor};
+  }
+`;
+
+export const SidebarProfileImage = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors.titleColor};
+  font-size: 1.5rem;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+`;
+
+export const SidebarProfileTextContainer = styled.div`
+  h3 {
+    margin: 0;
+  }
+
+  p {
+    color: ${({ theme }) => theme.colors.textColor};
+    margin: 0;
+  }
+`;
+
+export const SidebarProfileOptions = styled.div`
+  position: absolute;
+  inset: 0;
+  top: unset;
+  bottom: 12rem;
+  padding: 0.3rem 0.5rem;
+  transition: ${({ theme }) => theme.transition.fast};
+  background-color: ${({ theme }) => theme.colors.bodyColor};
+  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  opacity: ${({ showProfileOptions }) => (showProfileOptions ? "1" : "0")};
+  overflow: ${({ showProfileOptions }) =>
+    showProfileOptions ? "visible" : "hidden"};
+
+  ::after {
+    content: "";
+    width: 0;
+    height: 0;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-top: 10px solid ${({ theme }) => theme.colors.borderColor};
+    position: absolute;
+    left: 40%;
+  }
+`;
+
+export const SidebarProfileOptionsHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
+
+  i.bx-check {
+    color: ${({ theme }) => theme.colors.primaryColor};
+    font-size: 1.5rem;
+  }
+`;
+
+export const SidebarProfileOptionsBody = styled.div`
+  p {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.textColor};
+    transition: ${({ theme }) => theme.transition.fast};
+
+    :hover {
+      color: ${({ theme }) => theme.colors.titleColor};
+    }
+  }
+`;
