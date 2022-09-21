92c3e8ceb33b7d2c4d7b9a5090fa323cc93bf7f6
oomaar <omaar_5@hotmail.com>
2022-09-21 20:30:47 +0200

    setup search query api call

--- src/Screens/ThirdParty/ThirdParty.js
+++ src/Screens/ThirdParty/ThirdParty.js
@@ -1,20 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { format } from "date-fns";
 import {
   ImageCard,
   ImagesContainer,
   ThirdPartyContainer,
+  SearchContainer,
+  SearchInputContainer,
+  SearchIcon,
+  SearchInput,
+  SearchButton,
 } from "./styledThirdParty";
 
 export const ThirdParty = () => {
-  const string = `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
-
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(false);
-  console.log(
-    "🚀 ~ file: ThirdParty.js ~ line 9 ~ ThirdParty ~ loading",
-    loading
-  );
+  const [toggleSearch, setToggleSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
+
+  const searchURL = `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+  const string = `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+  const search = () => setIsSearching((state) => !state);
+
+  const getSearchResponse = async () => {
+    const response = await fetch(`${searchURL}`);
+    const data = await response.json();
+    setImages(data.results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getSearchResponse();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSearching]);
 
   useEffect(() => {
     const getResponse = async () => {
@@ -22,21 +42,55 @@ export const ThirdParty = () => {
       const response = await fetch(`${string}`);
       const data = await response.json();
       setImages(data);
-      console.log(
-        "🚀 ~ file: ThirdParty.js ~ line 15 ~ getResponse ~ data",
-        data
-      );
       setLoading(false);
     };
 
     getResponse();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const inputElement = useRef(null);
+
+  useEffect(() => {
+    toggleSearch && inputElement.current?.focus();
+  }, [toggleSearch]);
+
+  const searchUI = (
+    <SearchContainer onSubmit={(e) => e.preventDefault()}>
+      <SearchInputContainer toggleSearch={toggleSearch}>
+        <SearchIcon onClick={() => setToggleSearch((state) => !state)}>
+          <i className="bx bx-search-alt-2" />
+        </SearchIcon>
+        <SearchInput
+          ref={inputElement}
+          toggleSearch={toggleSearch}
+          type="search"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <SearchButton
+          toggleSearch={toggleSearch}
+          onClick={search}
+          type="submit"
+        >
+          search
+        </SearchButton>
+      </SearchInputContainer>
+    </SearchContainer>
+  );
+
   if (loading) {
-    return <h1>Loading....</h1>;
+    return (
+      <div>
+        {searchUI}
+        <h1>Loading...</h1>
+      </div>
+    );
   } else {
     return (
       <ThirdPartyContainer>
+        {searchUI}
         <ImagesContainer>
           {images.map((image) => (
             <ImageCard key={image.id}>
--- src/Screens/ThirdParty/styledThirdParty.js
+++ src/Screens/ThirdParty/styledThirdParty.js
@@ -2,6 +2,56 @@ import styled from "styled-components";
 
 export const ThirdPartyContainer = styled.div``;
 
-export const ImagesContainer = styled.div``;
+export const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
+  margin-bottom: 3rem;
+  padding-bottom: 1rem;
+`;
+
+export const SearchInputContainer = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid
+    ${({ toggleSearch, theme }) =>
+      toggleSearch ? theme.colors.titleColor : "transparent"};
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  height: 55px;
+  width: ${({ toggleSearch }) => (toggleSearch ? "500px" : "50px")};
+  transition: ${({ theme }) => theme.transition.fast};
+`;
+
+export const SearchIcon = styled.div`
+  color: ${({ theme }) => theme.colors.titleColor};
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+export const SearchInput = styled.input`
+  height: 100%;
+  width: ${({ toggleSearch }) => (toggleSearch ? "100%" : "0%")};
+  color: ${({ theme }) => theme.colors.titleColor};
+  outline: 0;
+  border: 0;
+  background: transparent;
+  padding-left: 1rem;
+  font-size: 1.5rem;
+  transition: ${({ theme }) => theme.transition.fast};
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.titleColor};
+  }
+`;
+
+export const SearchButton = styled.button``;
+
+export const ImagesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
 
 export const ImageCard = styled.div``;
