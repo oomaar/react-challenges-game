51935b458c88a89700f473569aa63745e07cc868
oomaar <omaar_5@hotmail.com>
2022-09-22 22:42:45 +0200

    create Pagination component

---
+++ src/Componetns/Pagination/Pagination.js
@@ -0,0 +1,101 @@
+import { range } from "lodash";
+import { PaginationButton, PaginationContainer } from "./styledPagination";
+
+export const Pagination = ({ pagesCount, setPageNumber, pageNumber }) => {
+  const numberOfPages = range(1, pagesCount);
+  const visibleCount = 5;
+
+  const getSliceStart = () => {
+    if (range(0, Math.ceil(visibleCount / 2)).includes(pageNumber)) {
+      return numberOfPages[0] - 1;
+    } else if (
+      range(
+        numberOfPages.length - Math.floor(visibleCount / 2),
+        numberOfPages.length + 1
+      ).includes(pageNumber)
+    ) {
+      return numberOfPages[numberOfPages.length - 1 - visibleCount];
+    } else {
+      return pageNumber - Math.ceil(visibleCount / 2);
+    }
+  };
+  const getSliceEnd = () => {
+    if (
+      range(
+        numberOfPages.length - Math.floor(visibleCount / 2),
+        numberOfPages.length + 1
+      ).includes(pageNumber)
+    ) {
+      return numberOfPages[numberOfPages.length - 1];
+    } else if (range(0, Math.ceil(visibleCount / 2)).includes(pageNumber)) {
+      return visibleCount;
+    } else {
+      return pageNumber + Math.floor(visibleCount / 2);
+    }
+  };
+
+  return (
+    <PaginationContainer>
+      {/* previous button */}
+      <PaginationButton
+        onClick={() =>
+          setPageNumber(pageNumber > 1 ? pageNumber - 1 : pageNumber)
+        }
+      >
+        Prev
+      </PaginationButton>
+
+      {/* previous dots button */}
+      {pageNumber - 1 > Math.ceil(visibleCount / 2) && (
+        <PaginationButton
+          onClick={() =>
+            setPageNumber(
+              pageNumber - visibleCount > 0 ? pageNumber - visibleCount : 1
+            )
+          }
+        >
+          ...
+        </PaginationButton>
+      )}
+
+      {/* mapping through a sliced array of page numbers */}
+      {numberOfPages.slice(getSliceStart(), getSliceEnd()).map((number) => (
+        <PaginationButton
+          key={number}
+          active={pageNumber === number}
+          onClick={() => setPageNumber(number)}
+        >
+          {number}
+        </PaginationButton>
+      ))}
+
+      {/* next dots button */}
+      {pageNumber <
+        numberOfPages[numberOfPages.length - 1] -
+          Math.floor(visibleCount / 2) && (
+        <PaginationButton
+          onClick={() =>
+            setPageNumber(
+              pageNumber + visibleCount <= numberOfPages.length
+                ? numberOfPages[pageNumber + visibleCount - 1]
+                : numberOfPages[numberOfPages.length - 1]
+            )
+          }
+        >
+          ...
+        </PaginationButton>
+      )}
+
+      {/* next button */}
+      <PaginationButton
+        onClick={() =>
+          setPageNumber(
+            pageNumber < numberOfPages.length ? pageNumber + 1 : pageNumber
+          )
+        }
+      >
+        next
+      </PaginationButton>
+    </PaginationContainer>
+  );
+};
---
+++ src/Componetns/Pagination/styledPagination.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  margin: 1rem 0rem;
+  justify-content: flex-end;
+`;
+
+export const PaginationButton = styled.button`
+  border: 0;
+  outline: 0;
+  color: ${({ theme }) => theme.colors.titleColor};
+  background-color: ${({ theme, active }) =>
+    active ? theme.colors.primaryColor : theme.colors.scrollBarThumb};
+  padding: 0.3rem 0.5rem;
+  border-radius: 0.2rem;
+  margin: 0 0.1rem;
+  font-size: 1rem;
+  cursor: pointer;
+`;
