2e36159f9bd750900fcb38f3549b9565b889e7b9
oomaar <omaar_5@hotmail.com>
2022-09-23 23:20:45 +0200

    fix a logic & syntax error in the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { darkSlice } from "./darkModeSlice/darkModeSlice";
+import darkSlice from "./darkModeSlice/darkModeSlice";
 
 export const store = configureStore({
-  reducer: darkSlice,
+  reducer: {
+    darkSlice,
+  },
 });
