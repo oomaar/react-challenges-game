0a5fab1c6048baccf7f846aa93b58e07abe58e9d
oomaar <omaar_5@hotmail.com>
2022-09-27 19:31:51 +0200

    refactor ThemeContext basic Template

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -1,12 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-const ThemeContext = createContext();
-const ThemeUpdateContext = createContext();
+export const ThemeContext = createContext();
+export const ThemeUpdateContext = createContext();
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
+
+export function useThemeUpdate() {
+  return useContext(ThemeUpdateContext);
+}
 
 export const ThemeContextProvider = ({ children }) => {
   const [darkTheme, setDarkTheme] = useState(true);
 
-  const toggleDarkTheme = setDarkTheme((state) => !state);
+  const toggleDarkTheme = () => setDarkTheme((state) => !state);
 
   return (
     <ThemeContext.Provider value={darkTheme}>
