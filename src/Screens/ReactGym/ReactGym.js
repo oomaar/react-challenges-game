407b274da35b20c12e07c45786110c37aeab59a2
omaar <omaar_5@hotmail.com>
2022-12-24 19:15:04 +0200

    a use state exercise

--- src/Screens/ReactGym/ReactGym.js
+++ src/Screens/ReactGym/ReactGym.js
@@ -8,15 +8,32 @@ import {
 } from "./styledReactGym";
 
 export const ReactGym = () => {
-  const [count, setCount] = useState(4);
+  const [count, setCount] = useState(0);
+
+  function decrementCount() {
+    // Bad set for previous state
+    // setCount(count - 1);
+
+    // Good set for previous state
+    setCount((prev) => prev - 1);
+
+    // conc.. if we call setCount 2 times in a row with the bad set
+    // it should dec. count by 2 but this wont happen, the 2 set counts will just override each other
+    // if we call set count 2 time with good set.
+    // now it will work as expected, it will dec by 2
+  }
+
+  function incrementCount() {
+    setCount((prev) => prev + 1);
+  }
 
   return (
     <GYMContainer>
       <CounterContainer>
         <CounterText>{count}</CounterText>
         <CounterButtons>
-          <CounterButton>-</CounterButton>
-          <CounterButton>+</CounterButton>
+          <CounterButton onClick={decrementCount}>-</CounterButton>
+          <CounterButton onClick={incrementCount}>+</CounterButton>
         </CounterButtons>
       </CounterContainer>
     </GYMContainer>
