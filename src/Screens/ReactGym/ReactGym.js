983c9223423640256575ffe54e7c356cc943f8b6
omaar <omaar_5@hotmail.com>
2022-12-26 10:21:33 +0200

    use Effect example 2

--- src/Screens/ReactGym/ReactGym.js
+++ src/Screens/ReactGym/ReactGym.js
@@ -1,12 +1,30 @@
 import { StateCounter } from "./StateCounter/StateCounter";
 import { EffectOne } from "./Effect/EffectOne";
 import { GYMContainer } from "./styledReactGym";
+import { useEffect, useState } from "react";
 
 export const ReactGym = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Return in the useEffect is used to clean up what happened in the last mount,
+  // Return is called first then whatever inside the block of the useEffect will be executed
+  // conc. return is called when we unmount the component or when the component mounts
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <GYMContainer>
       <StateCounter />
-      <EffectOne />
+      {/* <EffectOne /> */}
+      <div>{windowWidth}</div>
     </GYMContainer>
   );
 };
