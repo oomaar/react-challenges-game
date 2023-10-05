a8f73ccc14eae745b86dda34c9808f2a4b6ceb2f
omaar <omaar_5@hotmail.com>
2023-10-05 12:16:59 +0300

    lean basics of map()

--- src/Screens/Javascript/Mapping/Mapping.tsx
+++ src/Screens/Javascript/Mapping/Mapping.tsx
@@ -1,5 +1,29 @@
+import { ArrayOfCars, buildArrayOfCars } from "../Javascript";
 import { MappingContainer } from "./styled-Mapping";
 
 export const Mapping = () => {
+  /*
+    ! methods
+    * set
+    * get
+    * delete
+    * clear
+    * has
+    ! Properties
+    * size
+    */
+  const myMap = new Map<"Yes" | "No", ArrayOfCars>();
+
+  for (let i = 0; i < buildArrayOfCars().length; i++) {
+    if (buildArrayOfCars()[i].year === "2023") {
+      myMap.set("Yes", buildArrayOfCars()[i]);
+    } else {
+      myMap.set("No", buildArrayOfCars()[i]);
+    }
+  }
+
+  console.log(myMap);
+  console.log(myMap.entries());
+
   return <MappingContainer>Mapping</MappingContainer>;
 };
